29158aab5223a80245a39eb73191f2cd859238b0
Hang Jin <Hang91>
2018-01-14 14:01:17 -0500

    add request page

---
+++ src/services/sendemail.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class SendEmailService {
+
+  constructor(private http: Http) { }
+
+  sendEmailForDemo(user:any) {
+    return this.http.post('/api/sendemailfordemo', user);
+    //console.log(user);
+  }
+  
+  sendEmailForBeta(user:any) {
+    return this.http.post('/api/sendemailforbeta', user);
+  }
+
+  sendEmailForContact(user:any) {
+    return this.http.post('/api/sendemailforcontact', user);
+  }
+
+}
