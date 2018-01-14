cef8a2eb38b2dac4e1960725a9d8c86a6e6ce671
Hang Jin <Hang91>
2018-01-14 12:37:10 -0500

    screen page

---
+++ src/app/home/screen/screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-screen',  
+  styleUrls: [ './screen.component.scss' ],
+  templateUrl: './screen.component.html'
+})
+export class ScreenComponent {
+    
+}
