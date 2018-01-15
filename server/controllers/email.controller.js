5784365b64e32219606e34cc6c14725642d54421
Hang Jin <Hang91>
2018-01-15 17:42:45 -0500

    modify send-email-controller

--- server/controllers/email.controller.js
+++ server/controllers/email.controller.js
@@ -8,22 +8,22 @@ function sendEmailRequest(req, res, next) {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'infoweraisethebarr@gmail.com', // generated ethereal user
-            pass: '0115HomepageTeam' // generated ethereal password
+            user: 'sender_email@gmail.com', // generated ethereal user
+            pass: 'sender_passwd' // generated ethereal password
         }
     });
 
     // setup email data with unicode symbols
     let mailOption1 = {
-        from: '"Raise the Barr" <infoweraisethebarr@gmail.com>', // sender address
+        from: '"Raise the Barr" <sender_email@gmail.com>', // sender address
         to: req.body.email, // list of receivers
         subject: 'do-not-reply Request Confirmation', // Subject line
         text: "Hello " + req.body.firstname + ", \n\nYou have requested a demo. We'll contact you soon.\n\nBest,\nRaise the Barr", // plain text body
     };
 
     let mailOption2 = {
-        from: '"Raise the Barr" <infoweraisethebarr@gmail.com>', // sender address
-        to: 'jinhang91@hotmail.com', // manager address
+        from: '"Raise the Barr" <sender_email@gmail.com>', // sender address
+        to: 'receiver_email', // manager address
         subject: 'Request email from user', // Subject line
         text: "A user requests a demo, user's email address is " + req.body.email + ". User name is " + req.body.firstname, // plain text body
     }
@@ -110,22 +110,22 @@ function sendEmailContact(req, res, next) {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'infoweraisethebarr@gmail.com', // generated ethereal user
-            pass: '0115HomepageTeam' // generated ethereal password
+            user: 'sender_email@gmail.com', // generated ethereal user
+            pass: 'sender_passwd' // generated ethereal password
         }
     });
 
     // setup email data with unicode symbols
     let mailOption1 = {
-        from: '"Raise the Barr" <infoweraisethebarr@gmail.com>', // sender address
+        from: '"Raise the Barr" <sender_email@gmail.com>', // sender address
         to: req.body.email, // list of receivers
         subject: 'do-not-reply Request Confirmation', // Subject line
         text: "Hello " + req.body.firstname + ", \n\nYou have left a message to us. We'll contact you soon.\n\nBest,\nRaise the Barr", // plain text body
     };
 
     let mailOption2 = {
-        from: '"Raise the Barr" <infoweraisethebarr@gmail.com>', // sender address
-        to: 'jinhang91@hotmail.com', // manager address
+        from: '"Raise the Barr" <sender_email@gmail.com>', // sender address
+        to: 'receiver_email@hotmail.com', // manager address
         subject: 'Message from user', // Subject line
         text: "A user leaves message to us: " + req.body.message + "\n User's email address is " + req.body.email + ". User name is " + req.body.firstname, // plain text body
     }
